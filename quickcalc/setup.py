96c464a7500548b0599cc3a9448c1e883fa2b870
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-07-11 09:52:44 -0500

    feat: Rename project from "software" to "quickcalc" and update dependencies
    This commit renames the project from "software" to "quickcalc" and updates the setup.py file accordingly. Additionally, the dependency for python3-tk is added to the install_requires section.

--- quickcalc/setup.py
+++ quickcalc/setup.py
@@ -3,13 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="software",
+    name="quickcalc",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "tk",  # Esta es la forma de especificar la dependencia para python3-tk
+    ],
     entry_points={
         "console_scripts": [
-            "software=main:main",
+            "quickcalc=main:main",
         ],
     },
 )
